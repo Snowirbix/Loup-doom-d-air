dae366318c23b1364ce62178cd59ce14c3c64fb3
Snowirbix <snowirbix@gmail.com>
2020-04-17 15:42:23 +0200

    Setup 2D Renderer

---
+++ Assets/Global_Scripts/ExtensionMethods.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    /**
+     * <summary>Query the component</summary>
+     */
+    public static T Q<T>(this GameObject obj)
+    {
+        return obj.GetComponent<T>();
+    }
+    
+    /**
+     * <summary>Get transform position</summary>
+     */
+    public static Vector3 position(this GameObject obj)
+    {
+        return obj.transform.position;
+    }
+    
+    /**
+     * <summary>Get transform rotation</summary>
+     */
+    public static Quaternion rotation(this GameObject obj)
+    {
+        return obj.transform.rotation;
+    }
+
+    public static Rigidbody2D SetVelocity(this Rigidbody2D rb, Vector2 velocity)
+    {
+        rb.velocity = velocity;
+        return rb;
+    }
+
+    public static Rigidbody2D SetIsKinematic(this Rigidbody2D rb, bool isKinematic)
+    {
+        rb.isKinematic = isKinematic;
+        return rb;
+    }
+}
