66a474ea37adb2a437fd609a0d5be5b4050f57f3
Snowirbix <snowirbix@gmail.com>
2020-04-18 14:03:20 +0200

    Parallax script

---
+++ Assets/Environment/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    float multiplier = 0.0f;
+
+    [SerializeField]
+    bool horizontalOnly = false;
+
+    private Transform cameraTransform;
+
+    private Vector3 startCameraPos;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        startCameraPos = cameraTransform.position;
+        startPos = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        var position = startPos;
+
+        if (horizontalOnly)
+            position.x += multiplier * (cameraTransform.position.x - startCameraPos.x);
+        else
+            position += multiplier * (cameraTransform.position - startCameraPos);
+
+        transform.position = position;
+    }
+}
