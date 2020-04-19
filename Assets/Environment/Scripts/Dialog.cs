8024af518fe0d43aebda6ea9fd78b561019347b3
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2020-04-19 18:28:10 +0200

    Dialog

---
+++ Assets/Environment/Scripts/Dialog.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typeSpeed = 0.2f;
+
+    public static Dialog instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typeSpeed);           
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    public void NextSentence()
+    {
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+    }
+}
