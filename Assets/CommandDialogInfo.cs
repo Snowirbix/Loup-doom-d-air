c31970b521b1d2acf07637c2c5dc030c664151fe
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2020-04-20 20:39:29 +0200

    Dialog and all prefabs for plateforms

---
+++ Assets/CommandDialogInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CommandDialogInfo : MonoBehaviour
+{
+    DialogTrigger dialog;
+    public string sentenceKeyboard;
+    public string sentenceGamepad;
+    
+    void Awake()
+    {
+        dialog = gameObject.Q<DialogTrigger>();
+
+        if(Gamepad.current != null)
+        {
+            dialog.listSentences.Add(sentenceGamepad);
+        }
+        else
+        {
+            dialog.listSentences.Add(sentenceKeyboard);
+        }
+
+    }
+
+}
