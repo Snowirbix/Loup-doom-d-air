fa2c4662bf013a07cd34fd7025eacfddb9306c8b
Snowirbix <snowirbix@gmail.com>
2020-04-21 01:14:48 +0200

    Imp

--- Assets/Enemies/Scripts/Claw.cs
+++ Assets/Enemies/Scripts/Claw.cs
@@ -36,9 +36,9 @@ public class Claw : MonoBehaviour
         if (playerIn)
         {
             Vector2 diff = player.gameObject.Position() - transform.position.XY();
-            Debug.Log("HIT");
             player.hp--;
             player.gameObject.Q<Rigidbody2D>().SetVelocity(diff.normalized * knockBackForce);
+            player.Hit();
         }
     }
 
