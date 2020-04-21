97def4f4defc50c62d9c1971c7e0589eb5f773d8
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2020-04-21 02:05:28 +0200

    end of the game

---
+++ Assets/endOfTheGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endOfTheGame : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
