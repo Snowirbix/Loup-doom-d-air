8739bfb79ff61ef7230dccce0f06cf6b3116f2cf
Snowirbix <snowirbix@gmail.com>
2020-12-20 13:23:04 +0100

    Pause Pls

---
+++ Assets/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private int i = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            i++;
+        }
+        if (i > 0)
+        {
+            i++;
+        }
+        if (i == 15)
+        {
+            Debug.Break();
+        }
+    }
+}
